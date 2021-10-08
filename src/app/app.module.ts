4d3894f8538c7173241af1944255ce39e7cfbe1e
Hesam T <tlemir55@gmail.com>
2021-10-08 16:20:16 +0300

    post list added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
@@ -11,9 +12,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { CreatePostComponent } from './posts/create-post/create-post.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, CreatePostComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CreatePostComponent,
+    PostListComponent,
+  ],
   imports: [
     BrowserModule,
     FormsModule,
@@ -22,6 +29,7 @@ import { CreatePostComponent } from './posts/create-post/create-post.component';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
