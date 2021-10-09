d2b2c355437f0119c6328c10992e7ab258e4359d
Hesam T <tlemir55@gmail.com>
2021-10-09 16:04:51 +0300

    nodemon installed

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controller/postController");
 
-router.get("/", controller.getPosts);
+router.get("/api/getPosts", controller.getPosts);
 
 module.exports = router;
